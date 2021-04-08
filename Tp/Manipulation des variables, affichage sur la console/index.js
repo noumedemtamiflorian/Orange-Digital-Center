35043b7413f8bcdd65dedea62dd02ee05beed5ea
noumedem <noumedemtamiflorian@gmail.com>
2021-04-08 14:03:15 +0200

    Manipulation des variables, affichage sur la console

---
+++ Tp/Manipulation des variables, affichage sur la console/index.js
@@ -0,0 +1,12 @@
+const a = 5
+// a = 6
+// Uncaught TypeError: Assignment to constant variable.
+let b
+// console.log(b);
+// undefined 
+// undefined doit souvent tres apparaitre en React Js
+// const a;
+// a = 6
+// console.log(a);
+// Uncaught SyntaxError: Identifier 'a' has already been declared
+// Avec une constante il faut obligatoirement la declarer.
