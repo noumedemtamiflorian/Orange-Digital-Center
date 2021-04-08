b744593718f279fa3023c6ce6147359397aef00f
noumedem <noumedemtamiflorian@gmail.com>
2021-04-08 14:34:21 +0200

    Exercice sur les boucles 2

--- Tp/Exercice sur les boucles/index.js
+++ Tp/Exercice sur les boucles/index.js
@@ -1,5 +1,10 @@
-var x = 10
-while (x <= 25) {
-    console.log("Ecrire x = " + x);
-    x += 2
-}
+// var x = 10
+// while (x <= 25) {
+//     console.log("Ecrire x = " + x);
+//     x += 2
+// }
+// while (x < 6) {
+//     console.log('Ecrire x = ' + x);
+// }
+// Uncaught ReferenceError: x is not defined
+// deux erreur on a pas incrementer et la variable x n'est pas declarer
