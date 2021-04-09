84571f54228aa9312f824946de8e7cace8924cf6
noumedem <noumedemtamiflorian@gmail.com>
2021-04-09 14:34:52 +0200

    Integration de React dans un site web

--- Tp/Solution de l'exercice sur le DOM/index.js
+++ Tp/Solution de l'exercice sur le DOM/index.js
@@ -1,18 +1,26 @@
 
-const list = document.getElementById('list')
+const list = document.querySelector('ul')
 const button = document.querySelector('button')
 const form = document.querySelector('form')
 
 form.onsubmit = function (event) {
+    // Bloquer la redirection du formulaire
     event.preventDefault()
-    let eltAdd = document.querySelector('input').value
+    // Recuperation du produit
+    let produit = document.querySelector('input').value
+    // Creation de la liste a ajouter
     let li = document.createElement('li')
-    li.appendChild(document.createTextNode(eltAdd))
+    li.appendChild(document.createTextNode(produit))
+    // Creation du boutton supprimer
     let btn = document.createElement("button")
+    btn.appendChild(document.createTextNode("Supprimer"))
+    li.appendChild(btn)
+    // Attachement de l'evenement onclick au boutton cree
     btn.onclick = function (event) {
         list.removeChild(li)
     }
-    btn.appendChild(document.createTextNode("Supprimer"))
-    li.appendChild(btn)
+    // Ajout du li a la liste
     list.appendChild(li)
+    // Vidage du formulaire
+    document.querySelector('input').value = ""
 }
